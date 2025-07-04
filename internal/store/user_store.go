730e3d0a7d119d17a3d22738693f62f1c58112ce
Mike McAvoy <218127259+mikemcavoydev@users.noreply.github.com>
2025-07-04 16:40:16 +0100

    feat: user store layer

---
+++ internal/store/user_store.go
@@ -0,0 +1,93 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+)
+
+type password struct {
+	plainText *string
+	hash      []byte
+}
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash password  `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type UserStore interface {
+	CreateUser(user *User) error
+	GetUserByUsername(username string) (*User, error)
+	UpdateUser(user *User) error
+}
+
+type PostgresUserStore struct {
+	db *sql.DB
+}
+
+func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
+	return &PostgresUserStore{
+		db: db,
+	}
+}
+
+func (s *PostgresUserStore) CreateUser(user *User) error {
+	query :=
+		`INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING id, created_at, updated_at`
+
+	err := s.db.QueryRow(query, user.Username, user.Email, user.PasswordHash).Scan(
+		&user.ID, &user.CreatedAt, &user.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *PostgresUserStore) GetUserByUsername(username string) (*User, error) {
+	user := &User{
+		PasswordHash: password{},
+	}
+
+	query :=
+		`SELECT id, username, email, password_hash, created_at, updated_at FROM users WHERE username = $1`
+
+	err := s.db.QueryRow(query, username).Scan(
+		&user.ID, &user.Email, &user.PasswordHash.hash, &user.CreatedAt, &user.UpdatedAt,
+	)
+	if err != sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *PostgresUserStore) UpdateUser(user *User) error {
+	query :=
+		`UPDATE users SET username = $1, email = $2, updated_at = CURRENT_TIMESTAMP WHERE id = $3 RETURNING updated_at`
+
+	result, err := s.db.Exec(query, user.Username, user.Email, user.ID)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
